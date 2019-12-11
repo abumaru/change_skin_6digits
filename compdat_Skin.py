1fa21fce9b2258e287963edb8bdd3aadb37f8af4
gonzalo Amaru Gutierrez Mamani <57144202+abumaru@users.noreply.github.com>
2019-12-11 11:45:42 +0100

    Add files via upload

---
+++ compdat_Skin.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 11 11:35:49 2019
+
+@author: GOGUT
+"""
+
+file=r"Y:\ressim\lavani-deep\dg2\simu2\box_cases\Compdat_test.txt"
+with open(file) as f:
+    data = f.read()
+print(data)
+data2=data.replace("0.0000","7.0000")
+print(data2)
