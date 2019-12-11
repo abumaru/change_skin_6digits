fa254c4846d856f70bf2f95b4240958aef75c508
gonzalo Amaru Gutierrez Mamani <57144202+abumaru@users.noreply.github.com>
2019-12-11 11:55:19 +0100

    Update compdat_Skin.py

--- compdat_Skin.py
+++ compdat_Skin.py
@@ -8,6 +8,6 @@ Created on Wed Dec 11 11:35:49 2019
 file=r"Y:\ressim\lavani-deep\dg2\simu2\box_cases\Compdat_test.txt"
 with open(file) as f:
     data = f.read()
-print(data)
+#print(data)
 data2=data.replace("0.0000","7.0000")
 print(data2)
